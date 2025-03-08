93456a11fcd2eb5e8d883f52b979fb189a26e511
luna <93258461+LunaDEV-net@users.noreply.github.com>
2025-03-08 13:22:09 +0100

    Update SHOW-Light-Level-on-BA.py

--- SHOW-Light-Level-on-BA.py
+++ SHOW-Light-Level-on-BA.py
@@ -1,3 +1,13 @@
 def on_button_event_a():
     basic.show_number(input.light_level())
 input.on_button_event(Button.A, input.button_event_click(), on_button_event_a)
+
+"""
+basic.set_led_color(0xffffff)
+
+"""
+
+"""
+Normal in Box ~10
+Mit Weißer LED >100
+"""
