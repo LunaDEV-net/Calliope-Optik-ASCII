b2e4ce3bf688a1060898c110f8d411be96fc4632
luna <93258461+LunaDEV-net@users.noreply.github.com>
2025-03-08 13:27:10 +0100

    Create BitBanging.py

---
+++ BitBanging.py
@@ -0,0 +1,17 @@
+t = 1000
+to_send = [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1]
+for i in range(8*2):
+    to_send.append(0) # Break
+index = 0
+
+def onEvery_interval():
+    global index
+    if to_send[index] == 1:
+        basic.set_led_color(0xff0000)
+    if to_send[index] == 0:
+        basic.turn_rgb_led_off()
+    index += 1
+    if index > len(to_send) - 1:
+        index = 0
+
+loops.every_interval(t, onEvery_interval)
